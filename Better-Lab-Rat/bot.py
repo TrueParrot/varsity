86b75511d5018cf161be41a50483fd71a17807d1
Matt W <foggyio@outlook.com>
2020-07-12 16:17:07 +0100

    general code cleanup
    Remove unnecessary newlines
    Made code more readable
    Only handle expected errors, don't handle all errors

--- Better-Lab-Rat/bot.py
+++ Better-Lab-Rat/bot.py
@@ -23,15 +23,15 @@ client.remove_command("help") # Remove default help command to replace with our 
 
 def execute_query(database, query):
     conn = sqlite3.connect(database) 
-    c = conn.cursor()
+    c    = conn.cursor()
     c.execute(query)
     conn.commit()
     c.close()
     conn.close()
 
 def db_query(database, query):
-    conn = sqlite3.connect(database)
-    c = conn.cursor()
+    conn   = sqlite3.connect(database)
+    c      = conn.cursor()
     c.execute(query)
     result = c.fetchall()
     c.close()
@@ -51,22 +51,19 @@ async def on_ready():
 @client.event
 async def on_message(message):
     await client.process_commands(message)
-    
     if message.channel.id == 439434153629319168:
         await message.delete()
-
     if message.author.id in cooldowns:
         pass
     else:
         cooldowns.append(message.author.id)
-        author = message.author
+        author  = message.author
         current = db_query("users.db", "SELECT message_weekly FROM users WHERE user_id = '%s'" % (author.id))
         if len(current) == 0:
             create_user(author)
             current = 0
         else:
             current = current[0][0]
-
         new = current + 1
         execute_query("users.db", "UPDATE users SET message_weekly = %s WHERE user_id = '%s'" % (new, author.id))
         await asyncio.sleep(60)
@@ -75,8 +72,7 @@ async def on_message(message):
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        pass
-    
+        pass    
     if isinstance(error, commands.MissingRequiredArgument):
         embed = discord.Embed(title="Invalid Syntax",
                               description=f"Usage: **!{ctx.command.usage}**",
@@ -92,45 +88,42 @@ def staff_check():
         staff_role = discord.utils.get(ctx.author.roles, name="Staff")
         if staff_role is None:
             return False
-        
-        return True
+        else: # While arguably you don't need this else, it makes the code more readable to have it
+            return True
     return commands.check(predicate)
 
 """Commands"""
 @client.command()
 async def accept(ctx):
     if ctx.channel.id == 439434153629319168:
-        role = discord.utils.get(ctx.guild.roles, name="Registered")
+        role       = discord.utils.get(ctx.guild.roles, name="Registered")
         role_level = discord.utils.get(ctx.guild.roles, name="Noob")
+        channel    = client.get_channel(439465475265658891)
         await ctx.author.add_roles(role, role_level)
-
-        channel = client.get_channel(439465475265658891)
         embed = discord.Embed(title="User Registered",
                               description=f"{ctx.author.name} successfully registered!",
                               color=0x55FF55)
         await channel.send(embed=embed)
-
         create_user(ctx.author)
 
 @client.command()
 @staff_check()
 async def promotions(ctx):
-    levels = db_query("users.db", "SELECT message_weekly, user_id FROM users ORDER BY message_weekly DESC")
+    levels      = db_query("users.db", "SELECT message_weekly, user_id FROM users ORDER BY message_weekly DESC")
     leaderboard = []
     for counter in range(0, 15):
         try:
             user = ctx.guild.get_member(levels[counter][1])
             if user is None:
-                user_id = "N/A"
-                level = levels[counter][0]
+                user_mention = "N/A"
+                level        = levels[counter][0]
             else:
-                user_id = user.mention
-                level = levels[counter][0]
-
-            leaderboard.append(f"**#{counter+1} {user_id}: Messages: {level}**")
+                user_mention = user.mention
+                level        = levels[counter][0]
+            leaderboard.append(f"**#{counter+1} {user_mention}: Messages: {level}**")
         except IndexError:
-            pass
-
+            break # No more users in list, no need to keep checking
+            
     embed = discord.Embed(title="Possible Promotions",
                           description="\n".join(leaderboard),
                           color=0xFFAA00)
@@ -146,18 +139,16 @@ async def submit(ctx, *, flag: str):
     else:
         embed = discord.Embed(title="Invalid Flag!",
                               description="Oof! What a bummer! Try Harder!",
-                              color=0xFF5555)
-        
+                              color=0xFF5555) 
     await ctx.send(embed=embed)
 
 # Punishment Commands
 @client.command()
 @staff_check()
 async def lock(ctx, time: int=None):
-    await ctx.message.delete()
-    
-    default = discord.utils.get(ctx.guild.roles, name="Registered")
-    perms = default.permissions
+    await ctx.message.delete() # Delete issued command
+    default             = discord.utils.get(ctx.guild.roles, name="Registered")
+    perms               = default.permissions
     perms.send_messages = False
     await default.edit(permissions=perms)
     if time == None:
@@ -170,7 +161,7 @@ async def lock(ctx, time: int=None):
                               description=f"The server has been locked by {ctx.author.name} for **{time} minutes**.",
                               color=0xFF5555)
         msg = await ctx.send(embed=embed)
-        await asyncio.sleep(time*60)
+        await asyncio.sleep(time*60) # Could use some sort of schedualer rather than the current implementation that holds a thread open
         perms.send_messages = True
         await default.edit(permissions=perms)
         await msg.delete()
@@ -178,13 +169,11 @@ async def lock(ctx, time: int=None):
 @client.command()
 @staff_check()
 async def unlock(ctx):
-    await ctx.message.delete()
-    
-    default = discord.utils.get(ctx.guild.roles, name="Staff")
-    perms = default.permissions
-    perms.send_messages=True
+    await ctx.message.delete() # Delete Issued Command
+    default             = discord.utils.get(ctx.guild.roles, name="Staff")
+    perms               = default.permissions
+    perms.send_messages = True
     await default.edit(permissions=perms)
-
     embed = discord.Embed(title="Server Unlocked",
                           description=f"The server has been unlocked by {ctx.author.name}",
                           color=0x55FF55)
@@ -193,37 +182,33 @@ async def unlock(ctx):
 @client.command(usage="kick <member> <reason>")
 @staff_check()
 async def kick(ctx, member: discord.Member, *, reason: str):
-    embed = discord.Embed(title="User Kicked",
-                          description=f"{member.name} was kicked from The Labs by {ctx.author.name} for **{reason}**",
-                          color=0xFF5555)
-
+    embed      = discord.Embed(title="User Kicked",
+                               description=f"{member.name} was kicked from The Labs by {ctx.author.name} for **{reason}**",
+                               color=0xFF5555)
     user_embed = discord.Embed(title="Kicked",
                                description=f"You were kicked from The Labs by {ctx.author.name} for **{reason}**",
                                color=0xFF5555)
     await ctx.send(embed=embed)
     try:
         await member.send(embed=user_embed)
-    except:
-        pass
-
+    except discord.Forbidden: # User Privacy Too High
+        pass 
     await member.kick(reason=f"[{ctx.author.name}]: {reason}")
 
 @client.command(usage="ban <member> <reason>")
 @staff_check()
 async def ban(ctx, member: discord.Member, *, reason: str):
-    embed = discord.Embed(title="User Banned",
-                          description=f"{member.name} was banned from The Labs by {ctx.author.name} for **{reason}**",
-                          color=0xFF5555)
-
+    embed      = discord.Embed(title="User Banned",
+                               description=f"{member.name} was banned from The Labs by {ctx.author.name} for **{reason}**",
+                               color=0xFF5555)
     user_embed = discord.Embed(title="Banned",
                                description=f"You were banned from The Labs by {ctx.author.name} for **{reason}**",
                                color=0xFF5555)
     await ctx.send(embed=embed)
-    try:
+    try discord.Forbidden: # User Privacy Too High
         await member.send(embed=user_embed)
     except:
         pass
-
     await member.ban(reason=f"[{ctx.author.name}]: {reason}")
     
 # Admin Commands
